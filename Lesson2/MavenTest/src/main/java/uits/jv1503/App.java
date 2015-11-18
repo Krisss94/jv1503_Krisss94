f25abb80db2a7d4b290ee4a37cf140f5bc81f1d3
Krisss94 <krisss9494@gmail.com>
2015-11-18 19:58:59 +0300

    created folder structure

---
+++ Lesson2/MavenTest/src/main/java/uits/jv1503/App.java
@@ -0,0 +1,13 @@
+package uits.jv1503;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
